09813fd448d4fc8a2f2110e5be7bdc54b861a326
ratala <talaia.gamedev@pm.me>
2025-02-16 16:01:59 -0500

    Integrate Chi into project with basic hello world

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module first-little-server
 
 go 1.22
+
+require github.com/go-chi/chi/v5 v5.2.1
--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func main() {
+	// A method name prefixed with new is a golang convention to indicate a constructor.
+	// It is usually preferable to use these constructor methods when available.
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", HelloHandler)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(EntryHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
@@ -18,15 +27,7 @@ func main() {
 	}
 }
 
-// EntryHandler is the entry point for all http requests.
-// It acts as the controller.
-func EntryHandler(writer http.ResponseWriter, request *http.Request) {
-	switch request.Method {
-	case "GET":
-		break
-	case "POST":
-		break
-	default:
-		break
-	}
+// HelloHandler sends "hello world" to the client.
+func HelloHandler(writer http.ResponseWriter, request *http.Request) {
+	_, _ = writer.Write([]byte("Hello World"))
 }
