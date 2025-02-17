21e9f977699d3b2ee241afeb4948524ef1fa8706
ratala <talaia.gamedev@pm.me>
2025-02-16 20:15:38 -0500

    Add graceful shutdown

--- application/app.go
+++ application/app.go
@@ -3,16 +3,20 @@ package application
 import (
 	"context"
 	"fmt"
+	"github.com/redis/go-redis/v9"
 	"net/http"
+	"time"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func NewApp() *App {
 	return &App{
 		router: LoadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 }
 
@@ -22,11 +26,39 @@ func (app *App) Start(ctx context.Context) error {
 		Handler: app.router,
 	}
 
-	err := server.ListenAndServe()
-
-	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+	redisErr := app.rdb.Ping(ctx).Err()
+	if redisErr != nil {
+		return fmt.Errorf("error when pinging redis: %w", redisErr)
 	}
 
-	return nil
+	// Wrap defer call in anonymous function because defer keyword does not work when returning an error.
+	defer func() {
+		if err := app.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("Starting server")
+
+	channel := make(chan error, 1)
+
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			channel <- fmt.Errorf("failed to start server: %w", err)
+		}
+
+		close(channel)
+	}()
+
+	select {
+	case err := <-channel:
+		return err
+	case <-ctx.Done():
+		shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer shutdownCancel()
+
+		return server.Shutdown(shutdownCtx)
+	}
 }
--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"first-little-server/application"
 	"fmt"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	app := application.NewApp()
 
-	err := app.Start(context.TODO())
+	ctx, cancelFunc := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancelFunc()
 
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
